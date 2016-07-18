f8db8733ab21067c5ad5c68231369c0d92e1b046
Daniel Watts <danbadge@gmail.com>
2016-07-18 14:22:18 +0100

    add new endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('200 OK');
 });
 
+app.get('/status', function (req, res) {
+  res.send('IT ALL GOOD');
+});
+
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
