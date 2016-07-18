6c887906d3c11662e63c718857d868a7af0d04b2
Daniel Watts <danbadge@gmail.com>
2016-07-18 14:05:31 +0100

    remove text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('200 OK, OK?');
+  res.send('200 OK');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
