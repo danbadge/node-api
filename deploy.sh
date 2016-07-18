6fa1685d5bdb3626ea0086ec59e50be4b46c4413
Daniel Watts <danbadge@gmail.com>
2016-07-18 14:33:15 +0100

    force push

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 heroku git:remote -a node-api-$ENVIRONMENT -r $ENVIRONMENT
 echo $SNAP_PIPELINE_COUNTER
-git push $ENVIRONMENT 0.0.$SNAP_PIPELINE_COUNTER:master
+git push -f $ENVIRONMENT 0.0.$SNAP_PIPELINE_COUNTER:master
 ./check.sh
