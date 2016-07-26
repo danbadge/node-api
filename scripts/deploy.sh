a14a288891c2f734e96f958c57f3c7a26aac7851
Daniel Watts <danbadge@gmail.com>
2016-07-26 11:13:22 +0100

    login to aws registry before deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,14 @@ set -e #exit on error
 app_id=node-api-$ENVIRONMENT
 version=0.0.$SNAP_PIPELINE_COUNTER
 
+# login heroku registry
 docker login --email=_ --username=_ --password=$(heroku auth:token) registry.heroku.com
+
+# login aws registry
+login_command=`aws ecr get-login`
+eval $login_command
+
 docker tag $REGISTRY/node-api:$version registry.heroku.com/$app_id/web
 docker push registry.heroku.com/$app_id/web #this does a deploy too
-# can you login to multiple registry?
 
 ./scripts/check.sh
