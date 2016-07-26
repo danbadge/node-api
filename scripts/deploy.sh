25e53631d7eb73626f79d1d067973d30e1cd6684
Daniel Watts <danbadge@gmail.com>
2016-07-26 09:11:34 +0100

    check was in the wrong place

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,4 +10,4 @@ docker tag $REGISTRY/node-api:$version registry.heroku.com/$app_id/web
 docker push registry.heroku.com/$app_id/web #this does a deploy too
 # can you login to multiple registry?
 
-./check.sh
+./scripts/check.sh
