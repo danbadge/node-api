ca89c5b36398b104d8205b548abb8d6b78d21c35
Daniel Watts <danbadge@gmail.com>
2016-07-26 11:23:06 +0100

    remove scripts

--- build-and-run-container.sh
+++
@@ -1,3 +0,0 @@
-docker build -t cheeky-api-image .
-
-docker run -d -p 3000:80 --name cheeky-api-app cheeky-api-image
--- teardown-container.sh
+++
@@ -1,5 +0,0 @@
-docker stop cheeky-api-app
-
-docker rm cheeky-api-app
-
-docker rmi cheeky-api-image
