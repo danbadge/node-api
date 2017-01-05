264bb7c25ead47092eefa95f17f40d62139b4039
Daniel Watts <danbadge@gmail.com>
2017-01-05 11:43:47 +0000

    changing output

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('200 OK');
+  res.send('200 OK LATEST');
 });
 
 app.get('/status', function (req, res) {
