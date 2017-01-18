0695fbc8de7bdc26db2830a857a9737068b11fb9
Daniel Watts <danbadge@gmail.com>
2017-01-18 23:23:34 +0000

    lets try that again

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -4,7 +4,9 @@ set -x #echo on
 
 ls -la
 
+COMMIT_SHA=${CI_BUILD_REF:-${BUILDKITE_COMMIT}}
+
 (echo ${HEROKU_USER} && echo ${HEROKU_PWD}) | heroku login
 
 heroku git:remote -a $HEROKU_APP_NAME -r $CI_ENVIRONMENT_NAME
-git push -f $CI_ENVIRONMENT_NAME $CI_BUILD_REF:master
+git push -f $CI_ENVIRONMENT_NAME $COMMIT_SHA:master
